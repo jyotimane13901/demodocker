76989eacacd93ac35f4307322b8f4fc971ec34a1
jyotimane13901 <netkejyoti9@gmail.com>
2026-03-06 16:19:26 +0530

    upload

---
+++ demodocker/src/test/java/com/cjc/springbootappdemo/DemodockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjc.springbootappdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemodockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
